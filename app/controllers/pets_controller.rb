ee8354d255706b75105e08c65c8fc342b2fd740c
Geovannapg12 <geo.p2334@gmail.com>
2022-06-30 15:51:25 -0700

    api endpoints trello card completed

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,24 @@
 class PetsController < ApplicationController
+
+  def index
+    pets = Pet.all
+    render json: pets
+  end
+
+  def create
+    pet = Pet.create(pet_params)
+    render json: pet
+  end
+
+def update
+  end
+
+  def destroy
+  end
+
 end
+
+private
+def pet_params
+    params.require(:pet).permit(:name, :age, :enjoys, :image)
+  end
--- spec/requests/pets_spec.rb
+++ spec/requests/pets_spec.rb
@@ -2,6 +2,42 @@ require 'rails_helper'
 
 RSpec.describe "Pets", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of pets" do
+      Pet.create(
+        name: 'Charlie',
+        age: 2,
+        enjoys: 'Walks in the park',
+        image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+      )
+      get '/pets'
+
+      pet = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(pet.length).to eq 1
+    end
+
   end
+  describe "POST/create" do
+    it "create a new pet" do
+      pet_params = {
+        pet: {
+        name: 'Charlie',
+        age: 2,
+        enjoys: 'Walks in the park',
+        image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+    }
+  }
+      post '/pets', params: pet_params
+
+      expect(response).to have_http_status(200)
+     pet = Pet.first
+     expect(pet.name).to eq 'Charlie'
+     expect(pet.age).to eq 2
+     expect(pet.enjoys).to eq 'Walks in the park'
+     expect(pet.image).to eq 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+
+    end
+
+  end
+
 end
