1199229d995097b2182c3b289087fb21b7c7463e
Geovannapg12 <geo.p2334@gmail.com>
2022-07-01 16:26:44 -0700

    api endpoints continued

--- spec/requests/pets_spec.rb
+++ spec/requests/pets_spec.rb
@@ -40,6 +40,25 @@ RSpec.describe "Pets", type: :request do
 
   end
 
+  describe 'DELETE /destroy' do
+    it 'can delete an existing pet' do
+      pet_params = {
+        pet: {
+          name: 'Charlie',
+        age: 2,
+        enjoys: 'Walks in the park',
+        image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+      post '/pets', params: pet_params
+      pet = Pet.first
+      delete "/pets/#{pet.id}"
+      pet= Pet.all
+      expect(response).to have_http_status(200)
+      expect(pet).to be_empty
+    end
+  end
+
   describe "PATCH/update" do
     it "update a pet already on the databse" do
       pet_params = {
